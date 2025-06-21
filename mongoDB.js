776afa942b48ff635c3c58ba3c431d4e0fefed6a
Junfromc <101047429+Junfromc@users.noreply.github.com>
2025-06-21 19:00:46 +0800

    feat(mongoDB.js): add 2 funcs createUserDoc and updateUserDoc to fulfill the POST request

--- mongoDB.js
+++ mongoDB.js
@@ -12,3 +12,21 @@ const mySchema = new mongoose.Schema({
   ],
   count: { type: Number, default: 0 },
 });
+const User = mongoose.model('User', mySchema);
+
+function createUserDoc(usernameArray, done){
+    User.create(usernameArray)
+    .then(data=>done(null, data))
+    .catch(err=>done(err))
+}
+function updateUserDoc(id, exercises, done){
+    User.findByIdAndUpdate(id, {$inc:{count: 1}, $push:{log:exercises}}, {new: true})
+    .then(date=>done(null, date))
+    .catch(err=>done(err))
+}
+
+
+
+exports.createUserDoc = createUserDoc;
+exports.updateUserDoc = updateUserDoc;
+exports.User = User;
